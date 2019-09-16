87a588402d44c152580b7b6e2fc5b8862eff4279
Hai <truonghai29012901@gmail.com>
2019-09-16 18:12:13 -0400

    Added Ocean Controller

---
+++ Assets/_Scripts/OceanController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanController : MonoBehaviour
+{
+    public float verticalSpeed = 0.05f;
+    public float resetPosition = 4.8f;
+    public float resetPoint = -4.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+    /// <summary>
+    /// This method moves the ocean down the screen by verticalSpeed
+    /// </summary>
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(0.0f, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+    /// <summary>
+    /// This method resets the ocean to the resetPosition
+    /// </summary>
+     void Reset()
+    {
+        transform.position = new Vector2(0.0f, resetPosition);
+    }
+    void CheckBounds()
+    {
+        if(transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
